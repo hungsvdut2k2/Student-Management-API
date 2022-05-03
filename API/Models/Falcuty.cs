44857f86fce4d9546f4463d4d0788009f272db2d
hungsvdut2k2 <hungsvdut@gmail.com>
2022-05-03 08:51:58 +0700

    rebuild database

--- API/Models/Falcuty.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace API.Models
-{
-    public class Falcuty
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public List<UserInformation> UserInformation { get; set; }
-    }
-}
